dd40295f6502f93acca739548b9e88d697c931da
latinawhocodes <alyssaroque01@gmail.com>
2020-03-29 18:54:14 -0400

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(timeString) {
+  const formatTime = parseInt(timeString, 10)
+
+  if (formatTime < 12) {
+    return "Good Morning";
+  } else if (formatTime > 12 && formatTime < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(string) {
+  let greeting = document.getElementById('greeting');
+  greeting.innerText = string;
+}
